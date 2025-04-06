42ed073f1b79f316cdb3dfb7f22e599a6b27acff
Ахмедов Нурбек Медетбаевич <n.akhmedov@uzdc.uz>
2025-04-06 18:19:55 +0500

    changes

--- Services/Features/Currency/CurrencyInvalidateScheduler.cs
+++ Services/Features/Currency/CurrencyInvalidateScheduler.cs
@@ -1,17 +1,22 @@
 ﻿using Coravel.Invocable;
+using Microsoft.Build.Framework;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using myuzbekistan.Shared;
+using System.Globalization;
 
 namespace myuzbekistan.Services;
 
 public class CurrencyInvalidateScheduler: IInvocable
 {
     private readonly ICurrencyService _currencyService;
+    private readonly ILogger<CurrencyInvalidateScheduler> _logger;
     private readonly AppDbContext _appDbContext;
 
-    public CurrencyInvalidateScheduler(ICurrencyService currencyService, IDbContextFactory<AppDbContext> dbContextFactory)
+    public CurrencyInvalidateScheduler(ICurrencyService currencyService, IDbContextFactory<AppDbContext> dbContextFactory,ILogger<CurrencyInvalidateScheduler> logger)
     {
         _currencyService = currencyService;
+        this._logger = logger;
         _appDbContext = dbContextFactory.CreateDbContext();
 
     }
@@ -20,7 +25,9 @@ public class CurrencyInvalidateScheduler: IInvocable
         await _currencyService.Invalidate();
 
         var currency = await _currencyService.GetUsdCourse();
-        decimal rate = decimal.Parse(currency.Rate.Replace(".", ","));
+        decimal rate = decimal.Parse(currency.Rate, CultureInfo.InvariantCulture);
+
+        _logger.LogWarning($"CurrencyInvalidateScheduler: {currency.Ccy} - {currency.Rate}");   
 
         await _appDbContext.Database.ExecuteSqlRawAsync(
             "UPDATE \"Contents\" SET \"PriceInDollar\" = ceil(\"Price\" / {0}) WHERE \"Price\" != 0",
